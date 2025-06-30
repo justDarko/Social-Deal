3f8c5ccaaca48b584e723a78ba77f7386321c39d
Darko <darkoangelovski666@gmail.com>
2025-06-30 18:43:14 +0200

    configured version catalog with needed dependencies

---
+++ app/build.gradle.kts
@@ -0,0 +1,88 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlin)
+    alias(libs.plugins.hiltAndroid)
+    alias(libs.plugins.kotlinSymbolProcessing)
+    alias(libs.plugins.compose.compiler) // Kotlin compiler needed from Kotlin version 2.0
+    alias(libs.plugins.kotlin.serialization)
+}
+
+android {
+    namespace = "com.example.socialdeal"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.socialdeal"
+        minSdk = 27
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    // Android core bundle ->
+    implementation(libs.bundles.androidx.core)
+
+    // Compose bundle ->
+    implementation(platform(libs.compose.bom))
+    implementation(libs.bundles.compose)
+
+    // Compose navigation ->
+    implementation(libs.navigation.compose)
+
+    // Hilt (DI) ->
+    // ksp = Kotlin Symbol Processing
+    // Kotlin versions below 1.9.0 still use kapt (Kotlin annotation processing tool)
+    // kapt allows us to use Java annotation processors for processors that don't
+    // support Kotlin. This is done by generating Java stubs that the processors can read.
+    // This operation is expensive. KSP solves that problem because it analyzes the Kotlin code
+    // right away without generating Java stubs.
+    implementation(libs.bundles.hilt)
+    ksp(libs.hilt.compiler)
+
+    // UI utils bundle ->
+    implementation(libs.bundles.media)
+
+    // Timber for logging ->
+    implementation(libs.timber)
+
+    // Unit tests bundle ->
+    implementation(libs.bundles.unit.test)
+
+    // Android tests bundle ->
+    implementation(libs.bundles.android.test)
+}
